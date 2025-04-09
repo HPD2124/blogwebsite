930f68a57b15fecf738c89b91266e0cfb6ae2a39
HPD2124 <harrisonparrish.d@gmail.com>
2025-04-09 01:40:33 -0700

    javascript added

---
+++ blog.js
@@ -0,0 +1,19 @@
+
+const backToTopButton = document.getElementById("backToTop");
+
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
